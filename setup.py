f089380340bcbf24df9edec3fe4b16dce6882975
Christopher Jalen Adatan <jadatan1019@gmail.com>
2020-05-24 02:11:52 -0700

    Implement good integration practices
    Tests outside application code.
    
    For details, see:
    https://docs.pytest.org/en/latest/goodpractices.html

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="bpm_supreme", packages=find_packages())
---
+++ tests/test_file_duplicates.py
@@ -0,0 +1,6 @@
+"""
+This test file determines if duplicate file detection is working as intended
+"""
+
+import pytest
+
