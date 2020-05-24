47108b460d70faeaa2fbe6e64e1c16493567b2a4
Christopher Jalen Adatan <jadatan1019@gmail.com>
2020-05-24 03:18:37 -0700

    (bpm_supreme): Add TypeError tests
    Check __init__ argument types

--- tests/test_file_duplicates.py
+++ tests/test_file_duplicates.py
@@ -3,4 +3,12 @@ This test file determines if duplicate file detection is working as intended
 """
 
 import pytest
-from src import bpm_supreme
+from src.bpm_supreme.classes.BpmSupreme import BpmSupreme
+from src.bpm_supreme.classes.BpmSupreme import Song
+from selenium.webdriver import Firefox
+
+class test_bpm_supreme:
+  def test___init__(self):
+    driver = "test"
+    with pytest.raises(TypeError) as exception_info:
+      obj = BpmSupreme(driver, "Username", "Password")
