fb01365da8973983e3a7a15b736b7a34c149d410
Christopher Jalen Adatan <jadatan1019@gmail.com>
2020-05-25 10:12:58 -0700

    Add Song() constructor tests

--- tests/test_BpmSupreme.py
+++ tests/test_BpmSupreme.py
@@ -7,6 +7,7 @@ from selenium.common import exceptions as selenium_exceptions
 from selenium.webdriver import Firefox
 
 from src.bpm_supreme.classes.BpmSupreme import BpmSupreme
+from src.bpm_supreme.classes.BpmSupreme import Song
 
 @pytest.fixture
 def account(username, password):
@@ -19,30 +20,52 @@ def account(username, password):
   print("Tearing down Selenium Firefox WebDriver")
   driver.quit()
 
-# Check BpmSupreme constructor argument validation
-def test_constructor():
-  with Firefox() as driver:
-    # Bad webdriver
-    with pytest.raises(selenium_exceptions.WebDriverException):
-      BpmSupreme("Driver", "Username", "Password")
-
-    # Bad username
-    with pytest.raises(TypeError):
-      BpmSupreme(driver, driver, "password")
-      driver.close()
-
-    # Bad password
-    with pytest.raises(TypeError):
-      BpmSupreme(driver, "username", driver)
-      driver.close()
-
-# Check login() and scroll_page()
-def test_login(account):
-  # Check login()
-  assert account.login() == True
+class TestBpmSupreme():
+  """
+  Class for testing BpmSupreme() methods
+  """
   
-  # Navigate to the download-history
-  account.driver.get("https://app.bpmsupreme.com/account/download-history")
+  # Check BpmSupreme constructor argument validation
+  def test_constructor(self):
+    with Firefox() as driver:
+      # Bad webdriver
+      with pytest.raises(selenium_exceptions.WebDriverException):
+        BpmSupreme("Driver", "Username", "Password")
 
-  # Check scroll_page()
-  assert account.scroll_page() == True
+      # Bad username
+      with pytest.raises(TypeError):
+        BpmSupreme(driver, driver, "password")
+        driver.close()
+
+      # Bad password
+      with pytest.raises(TypeError):
+        BpmSupreme(driver, "username", driver)
+        driver.close()
+
+  # Check login() and scroll_page()
+  def test_login(self, account):
+    # Check login()
+    assert account.login() == True
+    
+    # Navigate to the download-history
+    account.driver.get("https://app.bpmsupreme.com/account/download-history")
+
+    # Check scroll_page()
+    assert account.scroll_page() == True
+
+class TestSong():
+  """
+  Class for testing Song() methods
+  """
+  def test_constructor(self):
+    with Firefox() as driver:
+      # Bad driver
+      with pytest.raises(selenium_exceptions.WebDriverException):
+        Song("Driver", driver.create_web_element("element"))
+      
+      # Bad container
+      with pytest.raises(TypeError):
+        Song(driver, "WebElement")
+        driver.close()
+
+    
