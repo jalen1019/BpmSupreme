498e4c388c767faa10c33013127b269f19a262dc
Christopher Jalen Adatan <jadatan1019@gmail.com>
2020-05-26 05:26:05 -0700

    Add pytest commandline options
    * song_name
        - BpmSupreme song to download
    * download_dir
        - BpmSupreme download directory
        - Currently has no associated tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,8 @@ import pytest
 def pytest_addoption(parser):
   parser.addoption("--username", action="store", help="BpmSupreme username login", required=True)
   parser.addoption("--password", action="store", help="BpmSupreme password login", required=True)
+  parser.addoption("--song-name", action="store", help="BpmSupreme song to download", required=False)
+  parser.addoption("--download-dir", action="store", help="BpmSupreme download directory")
 
 @pytest.fixture
 def username(request):
@@ -11,3 +13,11 @@ def username(request):
 @pytest.fixture
 def password(request):
   return request.config.getoption("--password")
+
+@pytest.fixture
+def song_name(request):
+  return request.config.getoption("--song-name")
+
+@pytest.fixture
+def download_dir(request):
+  return request.config.getoption("--download-dir")
