7a1c4227c4ecdb31530b093b2e5e4ede8050b323
Christopher Jalen Adatan <jadatan1019@gmail.com>
2020-05-27 01:42:46 -0700

    Add --duplicate_song config option

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,9 @@ import pytest
 def pytest_addoption(parser):
   parser.addoption("--username", action="store", help="BpmSupreme username login", required=True)
   parser.addoption("--password", action="store", help="BpmSupreme password login", required=True)
-  parser.addoption("--bad-song-name", action="store", help="BpmSupreme song to download that is unable to be downloaded", required=False)
+  parser.addoption("--bad-song-name", action="store", help="Invalid BpmSupreme song to download", required=False)
   parser.addoption("--download-dir", action="store", help="BpmSupreme download directory")
+  parser.addoption("--duplicate-song", action="store", help="Song name to check duplicate of using directory from --download-dir option")
 
 @pytest.fixture
 def username(request):
@@ -21,3 +22,7 @@ def bad_song_name(request):
 @pytest.fixture
 def download_dir(request):
   return request.config.getoption("--download-dir")
+
+@pytest.fixture
+def duplicate_song(request):
+  return request.config.getoption("--duplicate-song")
