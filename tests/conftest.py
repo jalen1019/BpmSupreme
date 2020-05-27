565d99730762e0d4e6b3075130e187e749a44d0c
Christopher Jalen Adatan <jadatan1019@gmail.com>
2020-05-27 01:02:40 -0700

    Style: Rename config option
    Change the song-name -> bad-song-name

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import pytest
 def pytest_addoption(parser):
   parser.addoption("--username", action="store", help="BpmSupreme username login", required=True)
   parser.addoption("--password", action="store", help="BpmSupreme password login", required=True)
-  parser.addoption("--song-name", action="store", help="BpmSupreme song to download", required=False)
+  parser.addoption("--bad-song-name", action="store", help="BpmSupreme song to download that is unable to be downloaded", required=False)
   parser.addoption("--download-dir", action="store", help="BpmSupreme download directory")
 
 @pytest.fixture
@@ -15,8 +15,8 @@ def password(request):
   return request.config.getoption("--password")
 
 @pytest.fixture
-def song_name(request):
-  return request.config.getoption("--song-name")
+def bad_song_name(request):
+  return request.config.getoption("--bad-song-name")
 
 @pytest.fixture
 def download_dir(request):
