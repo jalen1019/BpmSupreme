0b509f6bd5ef5520e9267636e2af56d2aabe1dad
Christopher Jalen Adatan <jadatan1019@gmail.com>
2020-05-30 12:12:34 -0700

    Update build script

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
-setup(name="bpm_supreme", packages=find_packages())
+setup(
+    name="bpm_supreme", 
+    version="0.1",
+    package_dir={"":"src"},
+
+    scripts=["src/bpm_supreme/download.py"],
+    install_requires=["selenium>=3.141.0"]
+)
